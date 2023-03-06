98c5b14468bd843ae783cb3f4fc090f23775aa52
hounyj <Julien.houny@gmail.com>
2023-03-06 08:15:41 +0100

    first commit

---
+++ ConcreteDictionary.java
@@ -0,0 +1,12 @@
+public class ConcreteDictionary extends Dictionary {
+	@Override
+	public void add(String mot) {
+		// A compléter
+	}
+
+	@Override
+	public boolean find(String mot) {
+		// A compléter
+		return false;
+	}
+}
