fe6c1238b53980ee116acd66d7b40f7cb578d517
hounyj <Julien.houny@gmail.com>
2023-03-09 10:31:03 +0100

    Partie 1 fini

--- ConcreteDictionary.java
+++ ConcreteDictionary.java
@@ -1,38 +1,30 @@
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class ConcreteDictionary extends Dictionary {
-	Hashtable<Character,Hashtable<String,Integer>> Dico;
-	
+	ArrayList<String> Dico;
 	public ConcreteDictionary(){
-		Dico = new Hashtable<Character,Hashtable<String,Integer>>();
+		Dico = new ArrayList<String>();
 
 	}
 	@Override
 	public void add(String mot) {
-		char index = mot.charAt(0);
-		if (this.Dico.containsKey(index)){
-			this.Dico.get(index).put(mot, 1);
-		}else{
-			Hashtable<String,Integer> NewElem = new Hashtable<String,Integer>();
-			NewElem.put(mot, 1);
-			this.Dico.put(index, NewElem);
+		
+		if(!(this.Dico.contains(mot))){
+			this.Dico.add(mot);
 		}
 	}
 
 	@Override
 	public boolean find(String mot) {
-		boolean found = false;
-		Enumeration<Character> keys = this.Dico.keys();
-		char current = keys.nextElement();
-		while(keys.hasMoreElements() && !found){
-			if(Dico.get(current).containsKey(mot)){
-				found = true;
-			}else{
-				current = keys.nextElement();
-			}
-		}
+		return this.Dico.contains(mot);
+	}
 
-		return found;
+	@Override
+	public Iterator<String> wordIterator() {
+		
+		Iterator<String> it = this.Dico.iterator();
+		return it;
 	}
 }
