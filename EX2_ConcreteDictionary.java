337fe3acea5f6d1a2cd45e21cbe144f28fd72a1b
hounyj <Julien.houny@gmail.com>
2023-03-09 10:56:21 +0100

    partie 2

---
+++ EX2_ConcreteDictionary.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+
+
+public class EX2_ConcreteDictionary extends Dictionary {
+	static class Noed{
+        String value;
+        Noed left, right;
+
+        Noed(String val){
+            this.value = val;
+            left=null;
+            right=null;
+        }
+    }
+	Noed Dico;
+
+	@Override
+	public void add(String mot) {
+
+	}
+
+	@Override
+	public boolean find(String mot) {
+		
+	}
+
+}
