fac97eaf0f78492aeceb7f0a74b288c2752679dd
hounyj <Julien.houny@gmail.com>
2023-03-16 09:59:49 +0100

    avanceer

--- ConcreteDictionary.java
+++ ConcreteDictionary.java
@@ -5,11 +5,12 @@ public class ConcreteDictionary extends Dictionary {
 	static class Noed{
         String value;
         Noed left, right;
-
+        boolean end;
         Noed(String val){
             this.value = val;
-            left=null;
-            right=null;
+            this.end = false;
+            this.left=null;
+            this.right=null;
         }
     }
 	Noed Dico;
@@ -20,80 +21,50 @@ public class ConcreteDictionary extends Dictionary {
 
 	@Override
 	public void add(String mot) {
-        String index = mot.substring(0, 1);
-        if(this.Dico.value.isEmpty()){
-            Noed current = Dico;
-            Dico.value = mot.substring(0, 1);
-            for(int i =1; i < mot.length();i++){
-                Noed nextchar = new Noed(mot.substring(i, i+1));
-                System.out.println(nextchar.value);
-                current.left = nextchar;
-                current = current.left;
-            }
-            current.value = current.value+'*';
-        }else if(this.Dico.value.equals(index)){
-            Noed current = Dico.left;
-            for(int i =1; i < mot.length();i++){
-                Noed nextchar = new Noed(mot.substring(i, i+1));
-                if( (current.value.equals(nextchar.value)) && (current.left != null) ){
-                    current = current.left;
-                }else{
-                    if(current.left != null){
-                        System.out.println(nextchar.value);
-                        current.right = nextchar;
-                        current = current.right;
-                    }else{
-                        System.out.println(nextchar.value);
-                        current.left = nextchar;
-                        current = current.left;
-                    }
-                }
-            }
-            current.value = current.value+'*';
-        }else{
-            Noed current = Dico;
-            while(current.right != null || !(current.value.equals(index))){
-                current = current.right;
-            }
-            if(current.value.equals(index)){
-                for(int i =1; i < mot.length();i++){
-                    Noed nextchar = new Noed(mot.substring(i, i+1));
-                    if(current.left.value.equals(nextchar.value) || current.left.value.equals(nextchar.value +"*")){
-                        current = current.left;
-                    }else if(current.right.value.equals(nextchar.value) || current.right.value.equals(nextchar.value +"*")){
-                        current = current.right;
-                    }
-                    current.left = nextchar;
-                    current = current.left;
-                }
-                current.value = current.value+'*';
-            }
-            
-        }
+        Noed courant = this.Dico;
+        add_rec(mot, courant);
+        
 	}
+
+    public boolean add_rec(String mot, Noed courant){
+        if(!(mot.isEmpty())){
+            String index = mot.substring(0, 1);
+            if(courant.value.isEmpty()){
+
+                courant.value = index;
+                if(courant.left == null){
+
+                    Noed next = new Noed("");
+                    courant.left = next;
+                    add_rec(mot.substring(1), courant.left);
+                }else{
+                    
+                    add_rec(mot.substring(1), courant.left);
+                }
+            }else if(courant.value.compareTo(index) == 0){
+
+                if(courant.left == null){
+
+                    Noed next = new Noed("");
+                    courant.left = next;
+                    add_rec(mot.substring(1), courant.left);
+                }else{
+
+                    add_rec(mot.substring(1), courant.left);
+                }
+            }
+
+
+            return false;
+        }else{
+            
+            return true;
+        }
+    }
 
 	@Override
 	public boolean find(String mot) {
-		boolean found = false;
-        Noed current = Dico;
-        while(current.value.equals(mot.substring(0, 1))){
-            current = current.right;
-        }
-        if(current.value.equals(mot.substring(0, 1))){
-            for(int i =1; i <mot.length(); i++){
-                if(current.left.value.equals(mot.substring(i, i+1))){
-                    current = current.left;
-                }else if(current.right.value.equals(mot.substring(i, i+1))){
-                    current = current.right;
-                }
-            }
-            if(current.value.equals(mot.substring(mot.length()-1,mot.length())+ "*")){
-                found = true;
-            }
-            return found;
-        }else{
-            return found;
-        }
+        return false;
         
 	}
 
